ed107d3990ecd67797e7275e1ae09980486a333e
SAM <sameerkhan27560@gmail.com>
2025-11-11 17:18:18 +0530

    fix new method .env not work on render

--- Backend/src/controllers/otp.controller.js
+++ Backend/src/controllers/otp.controller.js
@@ -1,5 +1,6 @@
 import crypto from 'crypto';
 import Otp from '../models/otp.model.js';
+import User from '../models/user.model.js';
 import { sendEmail } from '../utils/sendEmail.js';
 
 export const sendOpt = async (req, res) => {
@@ -16,42 +17,41 @@ export const sendOpt = async (req, res) => {
       to: email,
       subject: 'FileHive Email Verification Code',
       html: `
-        <div style="font-family: 'Segoe UI', Arial, sans-serif; background-color: #f9fafb; padding: 40px 0;">
-          <div style="max-width: 480px; margin: auto; background: white; border-radius: 10px; box-shadow: 0 2px 10px rgba(0,0,0,0.08); overflow: hidden;">
-            <div style="background: linear-gradient(135deg, #4f46e5, #7c3aed); padding: 24px; text-align: center;">
-              <h1 style="color: #ffffff; margin: 0; font-size: 24px;">📂 FileHive</h1>
-              <p style="color: #e0e7ff; margin: 4px 0 0;">Cloud Storage Authentication</p>
-            </div>
+  <div style="font-family: Arial, sans-serif; background-color: #f9fafb; padding: 40px 0;">
+    <div style="max-width: 480px; margin: auto; background: white; border-radius: 10px; box-shadow: 0 2px 10px rgba(0,0,0,0.08); overflow: hidden;">
 
-            <div style="padding: 30px; text-align: left;">
-              <h2 style="color: #111827; font-size: 18px; margin-bottom: 10px;">Hi ${name}</h2>
-              <p style="color: #374151; font-size: 15px; line-height: 1.6;">
-                Use the following One-Time Password (OTP) to verify your email address for <b>FileHive – Cloud Storage</b>.
-                </b>.
-              </p>
+      <div style="background: linear-gradient(135deg, #4f46e5, #7c3aed); padding: 24px; text-align: center;">
+        <h1 style="color: #ffffff; margin: 0; font-size: 24px;"> 📂 FileHive</h1>
+        <p style="color: #e0e7ff; margin: 4px 0 0;">Cloud Storage Authentication</p>
+      </div>
 
-              <div style="text-align: center; margin: 30px 0;">
-                <div style="display: inline-block; background: #4f46e5; color: white; padding: 12px 30px; border-radius: 8px; font-size: 24px; font-weight: 600; letter-spacing: 4px;">
-                  ${otp}
-                </div>
-              </div>
+      <div style="padding: 30px;">
+        <h2 style="color: #111827; margin-bottom: 10px;">Hi ${name} </h2>
 
-              <p style="color: #6b7280; font-size: 14px;">
-                <b>This code is valid for <b>5 minutes</b>.<br>
-                If you didn’t request this, you can safely ignore this email.
-                Someone else might have entered your email by mistake.
-              </p>
-            </div>
+        <p style="color: #374151; font-size: 15px; line-height: 1.6;">
+          Use the following One-Time Password (OTP) to verify your email address for <b>FileHive – Cloud Storage</b>.
+        </p>
 
-            <div style="background: #f3f4f6; padding: 16px; text-align: center; font-size: 13px; color: #6b7280;">
-              <p style="margin: 0;">
-                © ${new Date().getFullYear()} FileHive – Cloud Storage
-                <br />Built with Resend ❤️ sam
-              </p>
-            </div>
+        <div style="text-align: center; margin: 30px 0;">
+          <div style="display: inline-block; background: #4f46e5; color: white; padding: 12px 30px; border-radius: 8px; font-size: 24px; font-weight: 600; letter-spacing: 4px;">
+            ${otp}
           </div>
         </div>
-      `,
+
+        <p style="color: #6b7280; font-size: 14px;">
+          This code is valid for <b>5 minutes</b>.<br />
+          If you didn’t request this, ignore this email.
+        </p>
+      </div>
+
+      <div style="background: #f3f4f6; padding: 16px; text-align: center; font-size: 13px; color: #6b7280;">
+        © ${new Date().getFullYear()} FileHive – Cloud Storage<br />
+        Built with Resend
+      </div>
+
+    </div>
+  </div>
+`,
     });
 
     res.json({ success: true, message: 'OTP sent successfully' });
